7018c36b13c90570de43c7a85e71781d5ab86869
Adrian Roman <aroman_ro@yahoo.com>
2020-03-20 16:41:20 +0200

    Removed obsolete comment, now there is normalization.

--- VQMCMolecule/VQMCOrbital.cpp
+++ VQMCMolecule/VQMCOrbital.cpp
@@ -17,20 +17,6 @@ namespace Orbitals
 			else
 				val += m_orb2(r);
 
-			// the problem is that the combined orbitals are not orthogonal
-			// they come from different atoms (from the last shell, the valence one)
-			// so they have overlap
-			// so you cannot simply normalize the combination by 1 / sqrt(2)
-			// you have to take into account the cross terms, too
-			// that could be done by using the computation of the overlap integrals from the Hartree-Fock project
-			// but I won't bother, I'll just make the 'average' between them and leave it like that
-
-			// for the whole wave function the normalization constant doesn't matter, because it cancels out in the formulae
-			// that's why you don't see the 1 / sqrt(N!) factor for the Slater determinant in the code, for example
-			// as for an individual component, keep in mind that we're trying to have a trial wavefunction as good as possible
-			// and that's it
-			// it's just a trial function - ideally it would be the one we're looking for, but if we would know it, we wouldn't have to do the computation :)
-
 			val *= norm;
 		}
 
